4af631e0057cdb7edc5430547b5d85122aed7a7c
Arfikait-Sirius <Arfikait.Sirius@gmail.com>
2022-09-11 01:25:51 +0900

    Learn Tina. - fn_judge() and fn_print().

--- dir_Girls/Tina/Tina.v
+++ dir_Girls/Tina/Tina.v
@@ -5,6 +5,11 @@ const label_target = "[    TARGET]: "
 const label_judge = "[     JUDGE]: "
 const label_print = "[     PRINT]: "
 
+__global (
+     girl_name string
+     skill_name string
+)
+
 //------------------------
 // :[ NAME ]:
 //     fn_set_girl_name
@@ -13,8 +18,9 @@ const label_print = "[     PRINT]: "
 //     Skill
 //------------------------
 pub fn fn_set_girl_name( name string ){
+     girl_name = name
 
-     println( "$label_girls_name$name" )
+     println( "$label_girls_name$girl_name" )
 
      return
 }
@@ -27,8 +33,41 @@ pub fn fn_set_girl_name( name string ){
 //     Skill
 //------------------------
 pub fn fn_set_skill_name( target string ){
+     skill_name = target
 
-     println( "$label_target${target}()" )
+     println( "$label_target${skill_name}()" )
+
+     return
+}
+
+//------------------------
+// :[ NAME ]:
+//     fn_judge
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
+pub fn fn_judge( judgement bool ){
+     
+     if judgement {
+          println( "${label_judge}OK" )
+     } else {
+          println( "${label_judge}NG - Please check ${skill_name}." )
+     }
+
+     return
+}
+
+//------------------------
+// :[ NAME ]:
+//     fn_print
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
+pub fn fn_print( result string ){
+
+     println( "$label_print$result" )
 
      return
 }
